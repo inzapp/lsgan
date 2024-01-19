6ae0060c95a0eba84b8e0ee03ebf0322186665c2
inzapp <inzapp@naver.com>
2024-01-19 09:15:42 +0900

    change kernel size of transposed convolution layer 3 to 4, add weight clipping constraint to discriminator

--- model.py
+++ model.py
@@ -31,6 +31,17 @@ import tensorflow as tf
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 
+class WeightClipping(tf.keras.constraints.Constraint):
+    def __init__(self, clip_value):
+        self.clip_value = clip_value
+
+    def __call__(self, w):
+        return tf.clip_by_value(w, -self.clip_value, self.clip_value)
+
+    def get_config(self):
+        return {'clip_value': self.clip_value}
+
+
 class Model:
     def __init__(self, generate_shape, latent_dim):
         self.generate_shape = generate_shape
@@ -58,33 +69,34 @@ class Model:
         x = self.dense(x, 1024, activation='leaky', bn=bn)
         x = self.dense(x, self.latent_rows * self.latent_cols * 256, activation='leaky', bn=bn)
         x = self.reshape(x, (self.latent_rows, self.latent_cols, 256))
-        x = self.conv2d_transpose(x, 256, 3, 2, activation='leaky', bn=bn)
-        x = self.conv2d_transpose(x, 256, 3, 1, activation='leaky', bn=bn)
-        x = self.conv2d_transpose(x, 256, 3, 2, activation='leaky', bn=bn)
-        x = self.conv2d_transpose(x, 256, 3, 1, activation='leaky', bn=bn)
-        x = self.conv2d_transpose(x, 128, 3, 2, activation='leaky', bn=bn)
-        x = self.conv2d_transpose(x,  64, 3, 2, activation='leaky', bn=bn)
+        x = self.conv2d_transpose(x, 256, 4, 2, activation='leaky', bn=bn)
+        x = self.conv2d_transpose(x, 256, 4, 1, activation='leaky', bn=bn)
+        x = self.conv2d_transpose(x, 256, 4, 2, activation='leaky', bn=bn)
+        x = self.conv2d_transpose(x, 256, 4, 1, activation='leaky', bn=bn)
+        x = self.conv2d_transpose(x, 128, 4, 2, activation='leaky', bn=bn)
+        x = self.conv2d_transpose(x,  64, 4, 2, activation='leaky', bn=bn)
         g_output = self.conv2d_transpose(x, self.generate_shape[-1], 1, 1, activation='tanh', bn=False)
         return g_input, g_output
 
     def build_d(self, bn):
         d_input = tf.keras.layers.Input(shape=self.generate_shape)
         x = d_input
-        x = self.conv2d(x,  64, 5, 2, activation='leaky', bn=bn)
-        x = self.conv2d(x, 128, 5, 2, activation='leaky', bn=bn)
-        x = self.conv2d(x, 256, 5, 2, activation='leaky', bn=bn)
-        x = self.conv2d(x, 512, 5, 2, activation='leaky', bn=bn)
+        x = self.conv2d(x,  64, 5, 2, activation='leaky', bn=bn, kernel_constraint=self.weight_clipping_constraint())
+        x = self.conv2d(x, 128, 5, 2, activation='leaky', bn=bn, kernel_constraint=self.weight_clipping_constraint())
+        x = self.conv2d(x, 256, 5, 2, activation='leaky', bn=bn, kernel_constraint=self.weight_clipping_constraint())
+        x = self.conv2d(x, 512, 5, 2, activation='leaky', bn=bn, kernel_constraint=self.weight_clipping_constraint())
         x = self.flatten(x)
         d_output = self.dense(x, 1, activation='linear', bn=False)
         return d_input, d_output
 
-    def conv2d(self, x, filters, kernel_size, strides, bn=False, activation='leaky'):
+    def conv2d(self, x, filters, kernel_size, strides, bn=False, activation='leaky', kernel_constraint=None):
         x = tf.keras.layers.Conv2D(
             strides=strides,
             filters=filters,
             padding='same',
             kernel_size=kernel_size,
             use_bias=False if bn else True,
+            kernel_constraint=kernel_constraint,
             kernel_initializer=self.kernel_initializer())(x)
         if bn:
             x = self.batch_normalization(x)
@@ -116,6 +128,9 @@ class Model:
 
     def kernel_initializer(self):
         return tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
+
+    def weight_clipping_constraint(self, clip_value=0.01):
+        return WeightClipping(clip_value=clip_value)
 
     def activation(self, x, activation):
         if activation == 'leaky':
